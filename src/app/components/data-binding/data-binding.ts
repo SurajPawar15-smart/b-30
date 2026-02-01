c340a750804fa03cce8ab51cd64e7f078d19ea7d
SurajPawar15-smart <supersuraj143@gmail.com>
2026-02-01 09:51:04 +0530

    interpolation+ property binding

--- src/app/components/data-binding/data-binding.ts
+++ src/app/components/data-binding/data-binding.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './data-binding.css',
 })
 export class DataBinding {
-
+  courseName: string = 'Angular';
+  courseDuration = '3 months';
+  latestVersion: string;
+  isActive: boolean = false;
+  myPlaceholdertext = 'Enter Surname';
+  inputType = 'radio';
+  myClassName = 'bg-primary p-3';
+  constructor() {
+    this.latestVersion = 'angular 20';
+  }
 }
