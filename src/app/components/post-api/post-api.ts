b2ba057668abf469671102ff7ef6c0a18425375d
SurajPawar15-smart <supersuraj143@gmail.com>
2026-02-02 01:32:24 +0530

    update delete added

--- src/app/components/post-api/post-api.ts
+++ src/app/components/post-api/post-api.ts
@@ -27,9 +27,13 @@ export class PostApi {
       .post('https://api.freeprojectapi.com/api/BusBooking/PostBusLocation', this.newLocationObj)
       .subscribe({
         next: (res: any) => {
-          alert('Location created');
+          alert('Location created ✅');
+          this.resetForm();
+          this.getAllLocation();
         },
-        error: (err: any) => {},
+        error: (err: any) => {
+          alert('Create failed ❌');
+        },
         complete: () => {},
       });
   }
@@ -37,19 +41,63 @@ export class PostApi {
   getAllLocation() {
     this.isLoading = true;
     this.errorMsg = '';
-    debugger;
+    //debugger;
     this.http.get('https://api.freeprojectapi.com/api/BusBooking/GetBusLocations').subscribe({
       next: (res: any) => {
-        debugger;
+        //debugger;
+        this.locationArray = res;
         alert('Get all the Location');
       },
       error: (err: any) => {
         console.error(err);
-        this.errorMsg = 'Failed to fetch locations';
+        this.errorMsg = 'Failed to fetch locations ❌';
       },
       complete: () => {
         this.isLoading = false;
       },
     });
+  }
+
+  onEdit(data: any) {
+    //this.newLocationObj.data;
+    this.newLocationObj = { ...data };
+  }
+  onUpdate() {
+    this.http
+      .post('https://api.freeprojectapi.com/api/BusBooking/PostBusLocation', this.newLocationObj)
+      .subscribe({
+        next: () => {
+          alert('Location updated ✅');
+          this.resetForm();
+          this.getAllLocation();
+        },
+        error: (err) => {
+          alert('Update failed ❌ ' + err.message);
+        },
+      });
+  }
+
+  onDelete(id: number) {
+    const isConfirm = confirm('Are you sure want to Delete ?');
+    if (isConfirm) {
+      this.http
+        .delete('https://api.freeprojectapi.com/api/BusBooking/DeleteBusLocation?locationId=' + id)
+        .subscribe({
+          next: (res: any) => {
+            alert('Location deleted');
+            this.getAllLocation();
+          },
+          error: () => {
+            alert('Delete failed ❌');
+          },
+        });
+    }
+  }
+  resetForm() {
+    this.newLocationObj = {
+      locationId: 0,
+      locationName: '',
+      code: '',
+    };
   }
 }
